8e04e7de7706a4a111c8e3bcc5e10f97bb80eb7c
Víctor Pereira Pujoni Marri <victor.pujon@mrv.com.br>
2022-04-02 01:30:33 -0300

    Add project files.

---
+++ JsonWebTokenApi/Controllers/AuthController.cs
@@ -0,0 +1,91 @@
+﻿using JsonWebTokenApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JsonWebTokenApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static UsuarioModel usuario = new UsuarioModel();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UsuarioModel>> Register(UsuarioDTO request)
+        {
+            CreatePasswordHash(request.Senha, out byte[] passwordHash, out byte[] passwordSalt);
+
+
+            usuario.Username = request.NomeUsuario;
+            usuario.PasswordHash = passwordHash;
+            usuario.PasswordSalt = passwordSalt;
+
+            return Ok(usuario);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UsuarioDTO request)
+        {
+            if (usuario.Username != request.NomeUsuario) return BadRequest("Usuário não encontrado");
+
+            if (!VerifyPasswordHash(request.Senha, usuario.PasswordHash, usuario.PasswordSalt)) return BadRequest("Senha errada");
+
+            string token = CreateToken(usuario);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(UsuarioModel usuario)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, usuario.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: credentials
+                 );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+    }
+}
---
+++ JsonWebTokenApi/Models/UsuarioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonWebTokenApi.Models
+{
+    public class UsuarioDTO
+    {
+        public string NomeUsuario { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+    }
+}
