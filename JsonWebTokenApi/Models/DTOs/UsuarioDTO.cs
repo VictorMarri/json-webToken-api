c170f2851236ea853a5ec24b3a118f8672f84232
Víctor Pereira Pujoni Marri <victor.pujon@mrv.com.br>
2022-04-06 15:26:32 -0300

    Aplicação de record no DTO

--- JsonWebTokenApi/Models/DTOs/UsuarioDTO.cs
+++ JsonWebTokenApi/Models/DTOs/UsuarioDTO.cs
@@ -1,8 +1,4 @@
 ﻿namespace JsonWebTokenApi.Models
 {
-    public class UsuarioDTO
-    {
-        public string NomeUsuario { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
-    }
+    public record UsuarioDTO(string NomeUsuario, string Senha);
 }
--- JsonWebTokenApi/Services/UserService.cs
+++ JsonWebTokenApi/Services/UserService.cs
@@ -14,6 +14,7 @@ namespace JsonWebTokenApi.Services
         public string GetMyName()
         {
             var result = string.Empty;
+
             if (_httpContextAccessor.HttpContext != null)
                 result = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
 
